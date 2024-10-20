6c6e40da456a2b50411983799afc82414aaa3e45
MarcelaGomez <marcelasaraigomezcanas@gmail.com>
2024-10-19 15:28:19 -1200

    Creacion de header

---
+++ src/app/Components/header/Header.tsx
@@ -0,0 +1,13 @@
+import style from './Header.module.css'
+
+const Header = () => {
+  return (
+    <>
+        <header className={style.header}>
+            <h1>Agende su cita</h1>
+        </header>
+    </>
+  )
+}
+
+export default Header
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
+import Header from "./Components/header/Header";
 
 export default function Home() {
   return (
     <>
-  
+    <Header/>
     </>
   );
 }
